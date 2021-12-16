b7f4b9206e125ccaa247170c4b1194eff24a15ab
Turngait <wulzin.t@gmail.com>
2021-12-16 12:29:30 +0400

    Add animation on buttons

--- src/components/controls/Button/index.tsx
+++ src/components/controls/Button/index.tsx
@@ -6,7 +6,7 @@ import './index.scss';
 
 const Button: React.FC<IButtonProps> = ({title, onClick, size}) => {
   return (
-    <button type="button" className={`button ${size}`} onClick={onClick}>{title}</button>
+    <button type="button" className={`button ${size}`} onClick={onClick}><span>{title}</span></button>
   )
 }
 
