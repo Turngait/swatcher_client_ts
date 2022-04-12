9f70c4dae76e23a080eecb7cfb7f526e4a41b5fa
Ailleurs <wulzin.t@gmail.com>
2022-04-12 22:22:52 +0400

    Add i18n to project

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,2 @@
-// export const API_URL = 'http://localhost:5000';
-export const API_URL = "https://rocky-escarpment-32544.herokuapp.com";
+export const API_URL = 'http://localhost:5000';
+// export const API_URL = "https://rocky-escarpment-32544.herokuapp.com";
--- src/pages/Main/components/SignIn/index.tsx
+++ src/pages/Main/components/SignIn/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import TextInput from '../../../../components/controls/TextInput';
 import Button from '../../../../components/controls/Button';
@@ -6,6 +7,7 @@ import Button from '../../../../components/controls/Button';
 import './index.scss';
 
 const SignIn: React.FC<{login: (email: string, pass: string)=> Promise<void>}> = ({ login }) => {
+  const { t } = useTranslation();
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
 
@@ -14,7 +16,7 @@ const SignIn: React.FC<{login: (email: string, pass: string)=> Promise<void>}> =
   }
   return (
     <form className='signInBox'>
-      <h3 className='signInBox__header'>Войти</h3>
+      <h3 className='signInBox__header'>{t('index.signin')}</h3>
       <TextInput type="email" name="email" placeholder="E-mail..." onChange={(event) => {setEmail(event.target.value)}}/>
       <TextInput type="password" placeholder="Пароль..." onChange={(event) => {setPass(event.target.value)}}/>
       <Button title="Go!" onClick={signInService}/>
