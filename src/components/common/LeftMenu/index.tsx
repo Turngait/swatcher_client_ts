97fa5b5394e0b20098590ab2c99493ca1c21236b
Ailleurs <wulzin.t@gmail.com>
2022-12-15 12:38:19 +0400

    Add i18n control

--- src/components/common/LeftMenu/index.tsx
+++ src/components/common/LeftMenu/index.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import i18n from 'i18n';
 import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+
+import Select from 'components/controls/Select';
 
 import './index.scss';
 
 const LeftMenu: React.FC = () => {
   const { t } = useTranslation();
+  const [lang, setLang] = useState(localStorage.getItem("lang") || 'ru');
+
+  useEffect(() => {
+    const lang = localStorage.getItem("lang");
+    if (!lang) {
+      i18n.changeLanguage('ru');
+      localStorage.setItem("lang", "ru");
+    }
+    else i18n.changeLanguage(lang);
+  }, []);
+
+  const langs = [
+    {
+      title: "En",
+      value: "en"
+    },
+    {
+      title: "Ru",
+      value: "ru"
+    }
+  ];
+
+  function changeLang(event: any) {
+    i18n.changeLanguage(event.target.value);
+    setLang(event.target.value);
+    localStorage.setItem("lang", event.target.value);
+  }
 
   return (
     <div className="leftMenu">
@@ -19,6 +49,9 @@ const LeftMenu: React.FC = () => {
           <NavLink to="/health" className="leftMenu__nav__item">{t('menu.health')}</NavLink>
           {/* <NavLink to="/profile" className="leftMenu__nav__item">{t('menu.profile')}</NavLink> */}
         </nav>
+        <div className="leftMenu__langBox">
+          <Select items={langs} defaultValue={lang} onChange={changeLang}/>
+        </div>
     </div>
   )
 }
